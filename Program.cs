50cf457120789b6a7a22b6ec50b8e12f0b7cb6a6
BuildTools <unconfigured@null.spigotmc.org>
2022-06-26 19:15:01 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ilo_Enkote_epiku
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
